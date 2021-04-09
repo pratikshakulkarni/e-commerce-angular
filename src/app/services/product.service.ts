6d1aab439c6e0d2fb13c8d6ce8abafb2e28d7aa6
Pratiksha Kulkarni <Kulkarnipratiksha1997@gmail.com>
2021-04-08 22:29:58 -0700

    get product categories dynamically

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,13 +3,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductService {
 
   private baseUrl = "http://localhost:8080/api/products";
+  private categoryUrl = "http://localhost:8080/api/product-category";
 
   constructor(private httpClient: HttpClient) { }
 
@@ -18,14 +21,30 @@ export class ProductService {
     //Need to create new url for ID
     const searcUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searcUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searcUrl).pipe(
       map(response=>response._embedded.products)
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response=>response._embedded.productCategory)
+    );
+
+  
+  }
+
 }
 
-interface GetResponse{
+interface GetResponseProducts{
   _embedded: {
     products:Product[];
+  }
+}
+
+interface GetResponseProductCategory{
+  _embedded: {
+    productCategory:ProductCategory[];
   }
 }
